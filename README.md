2ebe97b45dc969e55b3a58d4fd96433b44bafae8
prashant sikarwar <prashant97sikarwar@gmail.com>
2021-06-28 16:48:06 +0530

    frontend completed along with the mining and pagination

--- README.md
+++ README.md
@@ -31,4 +31,17 @@ make sure to activate the virtual environment
 ```
 export PEER=True && python3 -m backend.app
 ```
+**Run the frontend**
 
+In the frontend directory:
+```
+npm run start
+```
+
+**Seed the backend with data**
+
+Make sure to activate the virtual environment
+
+```
+export SEED_DATA=True && python3 -m backend.app
+```
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -2,20 +2,29 @@ import time
 import uuid
 
 from backend.wallet.wallet import Wallet
+from backend.config import MINING_REWARD, MINING_REWARD_INPUT
 
 class Transaction:
     """
     Document of an exchange in currency from a sender to one
     or more recipients.
     """
-    def __init__(self, sender_wallet, recipient, amount):
-        self.id = str(uuid.uuid4())[0:8]
-        self.output = self.create_output(
+    def __init__(
+        self,
+        sender_wallet=None,
+        recipient=None,
+        amount=None,
+        id=None,
+        output=None,
+        input=None
+    ):
+        self.id = id or str(uuid.uuid4())[0:8]
+        self.output = output or self.create_output(
             sender_wallet,
             recipient,
             amount
         )
-        self.input = self.create_input(sender_wallet, self.output)
+        self.input = input or self.create_input(sender_wallet, self.output)
 
     def create_output(self, sender_wallet, recipient, amount):
         """
@@ -60,12 +69,31 @@ class Transaction:
 
         self.input = self.create_input(sender_wallet, self.output)
 
+    def to_json(self):
+        """
+        Serialize the transaction.
+        """
+        return self.__dict__
+
+    @staticmethod
+    def from_json(transaction_json):
+        """
+        Deserialize a transaction's json representation back into a
+        Transaction instance
+        """
+        return Transaction(**transaction_json)
+
     @staticmethod
     def is_valid_transaction(transaction):
         """
         Validate a transaction.
         Raise an exception for invalid transactions.
         """
+        if transaction.input == MINING_REWARD_INPUT:
+            if list(transaction.output.values()) != [MINING_REWARD]:
+                raise Exception('Invalid mining reward')
+            return
+
         output_total = sum(transaction.output.values())
 
         if transaction.input['amount'] != output_total:
@@ -78,9 +106,23 @@ class Transaction:
         ):
             raise Exception('Invalid signature')
 
+    @staticmethod
+    def reward_transaction(miner_wallet):
+        """
+        Generate a reward transaction that award the miner.
+        """
+        output = {}
+        output[miner_wallet.address] = MINING_REWARD
+
+        return Transaction(input=MINING_REWARD_INPUT, output=output)
+
 def main():
     transaction = Transaction(Wallet(), 'recipient', 15)
     print(f'transaction.__dict__: {transaction.__dict__}')
+
+    transaction_json = transaction.to_json()
+    restored_transaction = Transaction.from_json(transaction_json)
+    print(f'restored_transaction.__dict__: {restored_transaction.__dict__}')
 
 if __name__ == '__main__':
     main()
